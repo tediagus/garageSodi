000e330b83ce9120b17a64eaa6715e4199ac86db
tediagus <tediagus@gmail.com>
2017-02-22 21:09:59 +0100

    ajout plugin camera

--- src/pages/profil/profil.ts
+++ src/pages/profil/profil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from "ionic-native";
 
 @Component({
   selector: 'page-profil',
@@ -9,7 +10,25 @@ import { NavController } from 'ionic-angular';
 
 export class ProfilPage {
   
+  public base64Image: string;
+
   constructor( public navCtrl: NavController) { }
 
+  changePicture(){
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth:1000,
+      targetHeight:1000,
+    }).then((imageData) => {
+
+      this.base64Image = "data:image/jpeg;base64n" + imageData;
+    }, (err) => {
+       console.log(err);
+    })
+
+  }
+
 }
+
+
 
